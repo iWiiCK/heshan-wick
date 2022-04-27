fea9a493279b90a47d07166c19686acbedad6235
Heshan Wickramaratne <heshanW@99x.io>
2022-04-27 13:23:58 +0530

    feat: first-about-me-added

--- src/Pages/HomeScreen/components/ThreeJsAboutMe/index.js
+++ src/Pages/HomeScreen/components/ThreeJsAboutMe/index.js
@@ -1,33 +1,50 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { Physics, usePlane } from '@react-three/cannon'
-import { Plane, Sparkles } from '@react-three/drei'
+import { Physics } from '@react-three/cannon'
+import { Sparkles, Text, OrbitControls } from '@react-three/drei'
+import theme from '../../../../styles/theme'
+import useWindowDimensions from '../../../../hooks/useWindowDimensions'
 
 const ThreeJsAboutMe = () => {
+    const titleRef = useRef();
+    const { width } = useWindowDimensions();
+    const [titleFontSize, setTitleFontSize] = useState(0.5)
+    const title = "Hello World!"
+    const description =
+        `I'm a 3rd year Software Engineering Undergraduate`;
 
-    const PlaneMesh = (props) => {
-        const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2.5, 0, 0], ...props }))
-        return (
-            <mesh ref={ref} position={[0, -6, 0]}>
-                <Plane args={[100, 10]}>
-                    <meshStandardMaterial
-                        color={"#3b3b3b"}
-                        metalness={1}
-                        roughness={0.15}
-                        envMapIntensity={0.9}
-                    />
-                </Plane>
-            </mesh>
-        )
-    }
+    useEffect(() => {
+        if (width < 300) setTitleFontSize(0.3)
+        else if (width < 450) {
+            setTitleFontSize(0.4)
+        }
+        else {
+            setTitleFontSize(0.5)
+        }
+    }, [width]);
 
     return (
-        <Canvas style={{ height: 600 }}>
+        <Canvas style={{ height: 600 }} >
             <Physics>
+                <OrbitControls
+                    enablePan={false}
+                    enableZoom={false}
+                    minPolarAngle={Math.PI / 2}
+                    maxPolarAngle={Math.PI / 2}
+                />
                 <ambientLight />
-                <pointLight position={[0, 800, 100]} intensity={100} color={"white"} />
-                <PlaneMesh />
-                <Sparkles scale={10} size={3} position={ [0,-2, 0]}/>
+                <pointLight position={[0, 800, 50]} intensity={100} color={"white"} />
+                <Sparkles scale={10} size={3} position={[0, -2, 0]} noise={3} />
+                <Sparkles scale={30} size={400} position={[0, 2, -3]} color={theme.backgroundGreen} count={5} />
+                <Sparkles scale={30} size={400} position={[0, 2, -3]} color={"blue"} count={5} />
+
+                {/* Description and Title 1 */}
+                <Text ref={titleRef} color="white" fontSize={titleFontSize} position={[0, 0, 2]}>
+                    {title}
+                </Text>
+                <Text color="white" fontSize={titleFontSize * 0.4} position={[0, -0.5, 2]} textAlign="center" maxWidth={titleFontSize * 5} >
+                    {description}
+                </Text>
             </Physics>
         </Canvas>
     )
---
+++ src/hooks/useWindowDimensions.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+const useWindowDimensions = () => {
+  const getWindowDimensions = () => {
+    const { innerWidth: width, innerHeight: height } = window;
+    return { width, height };
+  };
+
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions(getWindowDimensions());
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+};
+
+export default useWindowDimensions;
