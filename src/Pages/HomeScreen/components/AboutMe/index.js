d0a83fe40cd93a8cdf39a534662152f07a22bd6e
iWiiCK <heshanwick2000@gmail.com>
2023-08-31 12:34:15 +0530

    feat: detailes-changed

--- src/Pages/HomeScreen/components/AboutMe/index.js
+++ src/Pages/HomeScreen/components/AboutMe/index.js
@@ -3,7 +3,6 @@ import { CloseCircleOutlined } from "@ant-design/icons";
 
 import { SocialIcon } from "react-social-icons";
 import {
-  JavascriptPlain,
   Html5OriginalWordmark,
   Css3PlainWordmark,
   JavaOriginalWordmark,
@@ -25,6 +24,18 @@ import {
   TrelloPlain,
   UnityOriginal,
   XdLine,
+  SlackOriginal,
+  AmazonwebservicesOriginal,
+  AzureOriginal,
+  GooglecloudOriginal,
+  GoOriginalWordmark,
+  NestjsPlain,
+  MongodbOriginal,
+  PostgresqlPlain,
+  FirebasePlainWordmark,
+  DockerPlain,
+  TypescriptOriginal,
+  JavascriptOriginal,
 } from "devicons-react";
 
 import EducationExperienceItem from "../common/EducationExperienceItem";
@@ -32,8 +43,8 @@ import EducationExperienceItem from "../common/EducationExperienceItem";
 import "./styles.sass";
 
 const AboutMe = ({ setAboutMeVisible }) => {
-  const mainBio = `My Name is Heshan Wickramaratne. I'm a 3rd year Software Engineering
-        Undergraduate at University of Central Lancashire.`;
+  const mainBio = `My Name is Heshan Wickramaratne. I'm a BSc. First Class (Hons) in Software Engineering
+        Graduate from the University of Central Lancashire (UK).`;
   const myInterests = `I love to code, create 3D art, Creative code and sometimes mix all these up.
         I also Blog my work afterwards`;
 
@@ -54,7 +65,7 @@ const AboutMe = ({ setAboutMeVisible }) => {
         </section>
         <section className="connect-with-me-section">
           <label className="title">Let's Connect</label>
-          <div className="flex-container flex-center">
+          <div className="flex-container">
             <SocialIcon
               network="github"
               bgColor="white"
@@ -88,6 +99,13 @@ const AboutMe = ({ setAboutMeVisible }) => {
           <label className="title">Experience</label>
           <div>
             <EducationExperienceItem
+              org="Insighture"
+              designation="Software Engineer (Full-Stack)"
+              duration="Present"
+            >
+              {`Full stack development with Nest.js, React, AWS and Go lang`}
+            </EducationExperienceItem>
+            <EducationExperienceItem
               org="99x"
               designation="Trainee Software Engineer (Full-Stack)"
               duration="2021-2022"
@@ -108,55 +126,79 @@ const AboutMe = ({ setAboutMeVisible }) => {
         <section className="programming-languages-section">
           <label className="title">Programming Languages</label>
           <div className="flex-container">
-            <JavascriptPlain />
-            <Html5OriginalWordmark />
-            <Css3PlainWordmark />
-            <JavaOriginalWordmark />
-            <CplusplusOriginal />
-            <CsharpOriginal />
+            <JavascriptOriginal className="iwiick-icons" />
+            <TypescriptOriginal className="iwiick-icons" />
+            <Html5OriginalWordmark className="iwiick-icons" />
+            <Css3PlainWordmark className="iwiick-icons" />
+            <JavaOriginalWordmark className="iwiick-icons" />
+            <CplusplusOriginal className="iwiick-icons" />
+            <CsharpOriginal className="iwiick-icons" />
+            <GoOriginalWordmark className="iwiick-icons" />
           </div>
           <hr />
         </section>
         <section className="frameworks-section">
           <label className="title">Frameworks</label>
           <div className="flex-container">
-            <AndroidPlainWordmark />
-            <NodejsOriginal />
-            <ReactOriginalWordmark />
-            <ReduxOriginal />
-            <ThreejsOriginal />
+            <AndroidPlainWordmark className="iwiick-icons" />
+            <NodejsOriginal className="iwiick-icons" />
+            <ReactOriginalWordmark className="iwiick-icons" />
+            <ReduxOriginal className="iwiick-icons" />
+            <ThreejsOriginal className="iwiick-icons" />
             <div className="invert">
-              <UnityOriginal />
+              <UnityOriginal className="iwiick-icons" />
             </div>
+            <NestjsPlain className="iwiick-icons" />
+          </div>
+          <hr />
+        </section>
+        <section className="Cloud-section">
+          <label className="title">Cloud</label>
+          <div className="flex-container">
+            <AmazonwebservicesOriginal className="iwiick-icons" />
+            <AzureOriginal className="iwiick-icons" />
+            <GooglecloudOriginal className="iwiick-icons" />
+            <DockerPlain className="iwiick-icons" />
+          </div>
+          <hr />
+        </section>
+        <section className="DB-section">
+          <label className="title">Databases</label>
+          <div className="flex-container">
+            <MongodbOriginal className="iwiick-icons" />
+            <PostgresqlPlain className="iwiick-icons" />
+            <FirebasePlainWordmark className="iwiick-icons" />
           </div>
           <hr />
         </section>
         <section className="version-controlling-section">
           <label className="title">Version Controlling</label>
           <div className="flex-container">
-            <GitPlainWordmark />
-            <GithubOriginal />
+            <GitPlainWordmark className="iwiick-icons" />
+            <GithubOriginal className="iwiick-icons" />
           </div>
           <hr />
         </section>
         <section className="designing-section">
           <label className="title">3D modelling & Designing</label>
           <div className="flex-container">
-            <BlenderOriginal />
-            <FigmaOriginal />
-            <IllustratorLine />
-            <PhotoshopLine />
-            <XdLine />
-            <SassOriginal />
+            <BlenderOriginal className="iwiick-icons" />
+            <FigmaOriginal className="iwiick-icons" />
+            <IllustratorLine className="iwiick-icons" />
+            <PhotoshopLine className="iwiick-icons" />
+            <XdLine className="iwiick-icons" />
+            <SassOriginal className="iwiick-icons" />
           </div>
           <hr />
         </section>
         <section className="project-management-section">
           <label className="title">Project Management</label>
           <div className="flex-container">
-            <JiraOriginal />
-            <TrelloPlain />
+            <JiraOriginal className="iwiick-icons" />
+            <TrelloPlain className="iwiick-icons" />
+            <SlackOriginal className="iwiick-icons" />
           </div>
+          <hr />
         </section>
       </div>
     </div>
